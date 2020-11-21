f6ba34793ff640fafe897a5b46f574112b04d483
Louis Leonardo <leonardolouis2@gmail.com>
2020-11-21 14:07:05 +0700

    First Post

---
+++ FirstJava/src/App.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukan Nama : ");
+        String nama = sc.nextLine();
+
+        System.out.println(nama);
+    }
+}
