74ea70f339d9672074953f1a22bfd7211e8e9d29
Louis Leonardo <leonardolouis2@gmail.com>
2020-11-21 14:38:37 +0700

    add season 1

---
+++ season1/src/app.java
@@ -0,0 +1,90 @@
+public class app {
+    public static void main(String[] args) {
+        //		Tipe data
+//		String a = "abc";			
+//		int b = 1;
+//		char c = 'a';
+//		long d = 123;
+//		short e = 123;
+//		double f = 1.1;
+//		float g = 1.1f;
+//		boolean h = true;
+		
+//		Wrapper class
+//		Integer
+//		Character
+//		Long
+//		Short
+//		Double
+//		Float
+//		Boolean
+		
+//		Type Casting
+//		String x = "123";
+//		int y = Integer.parseInt(x);
+	
+//		increment,decrement & aritmatika
+//		int x = 1;
+//		System.out.println(x++);
+//		System.out.println(x);
+//		System.out.println("------------");
+//		int y = 2;
+//		System.out.println(y + 1);
+//		System.out.println(y);
+		
+//		if
+		// String kalimat = "a";
+		// if(kalimat.equals("a")) {
+		// 	System.out.println("ini logic");
+		// }
+		
+//		Relational,comparison & Truth Table
+//		int x = 2;
+//		if(x>=2) {
+//			System.out.println("benar");
+//		}else {
+//			System.out.println("salah");
+//		}
+		
+//		if(true||false) {
+//			System.out.println("benar");
+//		}else {
+//			System.out.println("salah");
+//		}
+	
+//		int x = 2;
+//		int y = 7;
+//		if(x<4&&y>9) {
+//			System.out.println("benar");
+//		}else {
+//			System.out.println("salah");
+//		}
+		
+//		boolean x = true;
+//		boolean y = false;
+//		
+//		if(x||y) {
+//			System.out.println("benar");
+//		}else {
+//			System.out.println("salah");
+//		}		
+		
+//		Looping
+//		Terbatas  -> for
+//		for(int i=0;i>10;i++) {
+//			System.out.println(i);
+//		}
+		
+//		tak terbatas -> while, do while
+
+//		while(x==1) {
+//			System.out.println("yey");
+//		}
+		
+//		int x = 1;
+//		do {
+//			System.out.println("makan");
+//			x++;
+//		}while(x>=10);
+    }
+}
