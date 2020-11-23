afa2e3422f35624d9e1ed3e4419318108a84a307
Louis Leonardo <leonardolouis2@gmail.com>
2020-11-23 23:32:13 +0700

    update method

--- method/src/App.java
+++ method/src/App.java
@@ -1,5 +1,40 @@
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[]args){ 
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String>kode=new ArrayList<>();
+        ArrayList<String>password=new ArrayList<>();
+
+        kode.add("AA11");
+        password.add("123456");
+
+        kode.add("KEN11");
+        password.add("jajajaja");
+
+        // View 
+        for(int i=0;i<kode.size();i++){
+            System.out.println(kode.get(i));
+            System.out.println(password.get(i));
+        }
+
+        System.out.print("Mau kode apa cuy ?");
+        String iniBang = scan.nextLine();
+
+        for(int i=0;i<kode.size();i++){
+            if(iniBang.equals(kode.get(i))){
+                System.out.print("mau jadi apa kodenya ?");
+                kode.set(i, scan.nextLine());
+
+                System.out.print("mau jadi apa passwordnya ?");
+                password.set(i, scan.nextLine());
+            }
+        }
+
+        for(int i=0;i<kode.size();i++){
+            System.out.println(kode.get(i));
+            System.out.println(password.get(i));
+        }
+
+        scan.close();
     }
 }
